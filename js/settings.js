5f372369c4215ee3780f07c900a1e6b404ec3972
bleistivt <bleistivt@users.noreply.github.com>
2016-05-27 05:56:48 +0200

    simplify

--- js/settings.js
+++ js/settings.js
@@ -1,9 +1,7 @@
 jQuery(function ($) {
     'use strict';
 
-    var oldColor = false,
-        green = '#86EA86',
-        progressColor = $('#Form_InfiniteScroll-dot-ProgressColor'),
+    var progressColor = $('#Form_InfiniteScroll-dot-ProgressColor'),
         changeColor = function () {
             var $this = $(this);
             $this.css('background-color', $this.val());
@@ -14,12 +12,7 @@ jQuery(function ($) {
     progressColor.on('keyup', changeColor);
 
     $('#Form_InfiniteScroll-dot-NavStyle').change(function () {
-        if ($(this).val() === '1') {
-            oldColor = progressColor.val();
-            progressColor.val(green);
-        } else if (progressColor.val() === green && oldColor) {
-            progressColor.val(oldColor);
-        }
+        progressColor.val($(this).val() === '1' ? '#86EA86' : '#38ABE3');
         updateColor();
     });
 
